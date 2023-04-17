c101be6a57f1c9fb93b4f168f498bd20848bab53
MrFelopes <feliperfragoso@gmail.com>
2023-04-17 16:50:24 -0300

    ai

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpNLGqlBC9IsKwrATyfGegB7pM1_CfrkA",
+  authDomain: "aulafirebase-bfef9.firebaseapp.com",
+  projectId: "aulafirebase-bfef9",
+  storageBucket: "aulafirebase-bfef9.appspot.com",
+  messagingSenderId: "824026759461",
+  appId: "1:824026759461:web:f11ac2a434ffbfd5e21acc"
+};
+
+const app = initializeApp(firebaseConfig);
+// const auth = app.auth();
+const auth = getAuth(app);
+export { app, auth };
---
+++ src/screens/RMGameScreen.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { Image, View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import { styles } from "../utils/styles";
+
+export default function RMGameScreen() {
+    const [personagem, setPersonagem] = useState(null);
+    const [personagens, setPersonagens] = useState([]);
+    const [totalPersonagens, setTotalPersonagens] = useState(1);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character')
+            .then((response) => response.json())
+            .then((json) => {
+                setTotalPersonagens(json.info.count);
+            })
+    }, []);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character/' + returnRandomNumber())
+            .then((response) => response.json())
+            .then((json) => {
+                setPersonagem(json);
+            })
+    }, [totalPersonagens]);
+
+    async function handlePersonagemVivoOuMorto(resposta) {
+        const isAlive = personagem.status === 'Alive';
+        if (resposta === isAlive) {
+            alert('Você acertou!');
+        } else {
+            alert('Você errou!');
+        }
+        //alert(isAlive ? 'Você acertou!' : 'Você errou!');
+        // fetch('https://rickandmortyapi.com/api/character/' + returnRandomNumber())
+        //     .then((response) => response.json())
+        //     .then((json) => {
+        //         setPersonagem(json);
+        //     })
+    }
+
+    const returnRandomNumber = () => {
+        let randomNumber = Math.floor(Math.random() * totalPersonagens) + 1;
+
+        // canoot return 0
+        if (randomNumber === 0) {
+            return 1;
+        }
+        return randomNumber;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Rick and Morty Game</Text>
+            <Text style={styles.subtitle}>Você sabe se o personagem está vivo?</Text>
+            {personagem && (
+                <View>
+                    <Image source={{ uri: personagem.image }} style={{ width: 200, height: 200 }} />
+                    <Text style={{ fontSize: 32, textAlign: "center", marginVertical: 20 }}>
+                        O/a personagem {personagem.name} está vivo/a/e?
+                    </Text>
+                    <View style={{ flexDirection: "row", gap: 20 }}>
+                        <Button
+                            mode="contained"
+                            onPress={() => handlePersonagemVivoOuMorto(true)}
+                        >SIM</Button>
+                        <Button
+                            mode="contained"
+                            onPress={() => handlePersonagemVivoOuMorto(false)}
+                        >NÃO</Button>
+                    </View>
+                </View>
+            )}
+        </View>
+    );
+}
